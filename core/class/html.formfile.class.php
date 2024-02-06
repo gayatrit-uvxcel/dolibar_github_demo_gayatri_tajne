08a07b4716fc6ed0bcaa1c612270b70e1b5266ae
Gayatri Tajne <gayatrit@uvxcel.com>
2024-02-06 10:20:06 +0530

    fix the bug pdf preview

--- core/class/html.formfile.class.php
+++ core/class/html.formfile.class.php
@@ -825,7 +825,11 @@ class FormFile
 					// Define relative path for download link (depends on module)
 					$relativepath = $file["name"]; // Cas general
 					if ($modulesubdir) {
+						$modulesubdir = preg_replace('/-01$/', '', $modulesubdir);
+						$modulesubdir = preg_replace('/A$/', '', $modulesubdir);
 						$relativepath = $modulesubdir . "/" . $file["name"]; // Cas propal, facture...
+
+						echo "path in for each ", $modulesubdir;
 					}
 					if ($modulepart == 'export') {
 						$relativepath = $file["name"]; // Other case
